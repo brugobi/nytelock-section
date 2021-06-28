ad2a361689c7f0b6ad6da6f1b45fd6b0029d0198
brugobi <brgobbi@hotmail.com>
2021-06-28 14:48:14 -0400

    Add single button to Types component

--- src/components/Types.js
+++ src/components/Types.js
@@ -1,7 +1,10 @@
+/* eslint-disable */
 import React from 'react';
 
-const Types = () => (
-  <div />
+const Types = ({ selectItem }) => (
+  <div>
+    <button onClick={() => selectItem('combination')}>combination</button>
+  </div>
 );
 
 export default Types;
