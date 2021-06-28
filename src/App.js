99760027516f382bb35f3129afb60aa488e71902
brugobi <brgobbi@hotmail.com>
2021-06-28 16:38:38 -0400

    Refactor: add all button dynamic and fix issue displaying the default item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import './App.css';
 import items from './data/data';
 import Types from './components/Types';
 import LogoItem from './components/LogoItem';
 
+const allTypes = [...new Set(items.map((item) => item.type))];
+
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState([items[0]]);
+  const [types] = useState(allTypes);
 
   const selectItem = (type) => {
     const newItems = items.filter((item) => item.type === type);
@@ -20,7 +21,7 @@ function App() {
         <div>
           <h2>our Logos</h2>
         </div>
-        <Types selectItem={selectItem} />
+        <Types types={types} selectItem={selectItem} />
         <LogoItem items={menuItems} />
       </section>
     </main>
--- src/components/Types.js
+++ src/components/Types.js
@@ -1,10 +1,21 @@
 /* eslint-disable */
 import React from 'react';
 
-const Types = ({ selectItem }) => (
-  <div>
-    <button onClick={() => selectItem('combination')}>combination</button>
-  </div>
-);
+const Types = ({ types, selectItem }) => {
+  return (
+    <div>
+      {types.map((type, index) => {
+        return (
+          <button
+            type="button"
+            className="btn-Type"
+            key={index}
+            onClick={() => selectItem(type)}> {type}
+          </button>
+        )
+      })}
+    </div>
+  );
+};
 
 export default Types;
