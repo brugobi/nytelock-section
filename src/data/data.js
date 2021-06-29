fb601fef9a5fe15ed0a831c4aa9313735fd144a9
brugobi <brgobbi@hotmail.com>
2021-06-29 11:46:14 -0400

    update the data because of a misspelling

--- src/data/data.js
+++ src/data/data.js
@@ -10,7 +10,7 @@ const items = [
     id: 1,
     title: 'Lettermark Logos',
     type: 'lettermark',
-    content: "A Monogram or lettermark is a typography-based logo that's comprised of a few letters, ussually a company's initials.The lettermark is all about simplicity. But utilizing just a few letters lettermark logos are effective at streamlining any company brand if they have a long name.",
+    content: "A Monogram or lettermark is a typography-based logo that's comprised of a few letters, usually a company's initials.The lettermark is all about simplicity. But utilizing just a few letters lettermark logos are effective at streamlining any company brand if they have a long name.",
     img: lettermark,
   },
   {
