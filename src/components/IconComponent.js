fe2c4c29b02ed3991a383404afd378e7c7d1c59a
brugobi <brgobbi@hotmail.com>
2021-06-30 14:49:38 -0400

    FIX(linters): fix linters issues and add PropTypes

--- src/components/IconComponent.js
+++ src/components/IconComponent.js
@@ -1,16 +1,19 @@
-/* eslint-disable */
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as Icons from 'react-icons/fa';
 
 const DynamicFaIcon = ({ icon }) => {
   const IconComponent = Icons[icon];
-
 
   if (!IconComponent) {
     return <Icons.FaBeer />;
   }
 
   return <IconComponent />;
+};
+
+DynamicFaIcon.propTypes = {
+  icon: PropTypes.string.isRequired,
 };
 
 export default DynamicFaIcon;
